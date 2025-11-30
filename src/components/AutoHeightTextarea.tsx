7dfb97622b784da1e5ef602ec73addaf08588e72
jjjuni <0210leejun@gmail.com>
2025-12-01 02:16:19 +0900

    ♻️ refactor: ref 수정

--- src/components/AutoHeightTextarea.tsx
+++ src/components/AutoHeightTextarea.tsx
@@ -1,58 +1,52 @@
-'use client'
+import { TextareaHTMLAttributes, useLayoutEffect, useRef, useState, forwardRef } from "react";
 
-import { TextareaHTMLAttributes, useLayoutEffect, useRef, useState } from "react";
-
-interface AutoHeightTextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+interface AutoHeightTextAreaProps extends Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'value'> {
+  value: string;
   minHeight?: number;
   maxHeight?: number;
-  value: string;
   spanClassName?: string;
   containerClassName?: string;
-  textareaRef?: React.RefObject<HTMLTextAreaElement | null>
 }
 
-export const AutoHeightTextarea: React.FC<AutoHeightTextAreaProps> = ({
-  minHeight = 100,
-  maxHeight = 500,
-  value: inputValue,
-  spanClassName,
-  containerClassName,
-  textareaRef,
-  ...props
-}) => {
-  const [textareaHeight, setTextareaHeight] = useState(minHeight);
-  const spanRef = useRef<HTMLSpanElement>(null);
+export const AutoHeightTextarea = forwardRef<HTMLTextAreaElement, AutoHeightTextAreaProps>(
+  ({ minHeight = 100, maxHeight = 500, value: inputValue, spanClassName, containerClassName, ...props }, ref) => {
+    const [textareaHeight, setTextareaHeight] = useState(minHeight);
+    const spanRef = useRef<HTMLSpanElement>(null);
 
-  useLayoutEffect(() => {
-    if (spanRef.current) {
-      const textHeight = spanRef.current.getBoundingClientRect().height;
-      const calculatedHeight = Math.min(Math.max(minHeight, textHeight), maxHeight);
-      setTextareaHeight(calculatedHeight);
-    }
-  }, [inputValue, minHeight]);
+    useLayoutEffect(() => {
+      if (spanRef.current) {
+        const textHeight = spanRef.current.getBoundingClientRect().height;
+        const calculatedHeight = Math.min(Math.max(minHeight, textHeight), maxHeight);
+        setTextareaHeight(calculatedHeight);
+      }
+    }, [inputValue, minHeight]);
 
-  return (
-    <div className={`${containerClassName} relative`} style={{ width: `100%`, height: `${textareaHeight}px` }}>
-      <span
-        ref={spanRef}
-        className={`${props.className} ${spanClassName}`}
-        style={{
-          pointerEvents: "none",
-          position: "absolute",
-          whiteSpace: "pre-wrap",
-          wordBreak: "break-word",
-          opacity: 0,
-        }}>
-        {inputValue || ""}
-        {inputValue?.endsWith("\n") ? "\u00A0" : null}
-      </span>
-      <textarea
-        ref={textareaRef}
-        className={`${props.className}`}
-        style={{ height: `${textareaHeight}px` }}
-        value={inputValue}
-        {...props}
-      />
-    </div>
-  );
-};
+    return (
+      <div className={`${containerClassName} relative`} style={{ width: `100%`, height: `${textareaHeight}px` }}>
+        <span
+          ref={spanRef}
+          className={`${props.className} ${spanClassName}`}
+          style={{
+            pointerEvents: "none",
+            position: "absolute",
+            whiteSpace: "pre-wrap",
+            wordBreak: "break-word",
+            opacity: 0,
+          }}
+        >
+          {inputValue || ""}
+          {inputValue?.endsWith("\n") ? "\u00A0" : null}
+        </span>
+        <textarea
+          ref={ref}
+          className={`${props.className}`}
+          style={{ height: `${textareaHeight}px` }}
+          value={inputValue}
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+AutoHeightTextarea.displayName = "AutoHeightTextarea";
